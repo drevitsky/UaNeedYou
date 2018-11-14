1ac51c607c3be12b12ddbae66cf0b3def5bbf45e
drevitsky <4227723@gmail.com>
2018-11-14 14:16:31 +0200

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+// var module = document.querySelector(".article__content");
+
+// $clamp(module, {clamp: 5,
+//                 truncationHTML: '<a href="#">more...</a>'
+
+//                 });
+
+	 
+})
