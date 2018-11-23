d875a99e4c9ba8927281172d428b2ff2a08e4e4b
drevitsky <4227723@gmail.com>
2018-11-23 20:26:10 +0200

    create team

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,29 +1,26 @@
 $(document).ready(function(){
-// var module = document.querySelector(".article__content");
 
-// $clamp(module, {clamp: 5,
-//                 truncationHTML: '<a href="#">more...</a>'
-
-//                 });
-
+// делаем разворачивающуюся статью
 $('.article__more').on('click', function() {
     $(this).siblings('div.article__content').toggleClass('active');
     // console.log();
 	});
 
+
+//делаем активное меню
 var location = window.location.href;
-    var cur_url = location.split('/').pop();
+var cur_url = location.split('/').pop();
     console.log('location = ' + location);
     console.log('cur_url = ' + cur_url);
 
-$('.nav-link').each(function() {
+$('#nav_menu .nav-link').each(function() {
     var link = $(this).attr('href');
  	console.log('link=' + link);
  	console.log('this=' + this);
         if (cur_url == link)
         {
-            $(this).find('.nav-link').addClass('active');
-
+            $(this).addClass('active');
+            console.log('addClass');
         }
 	});
 	 
