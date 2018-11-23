9047c31921864facf34d65c168ae770ee2db1f59
drevitsky <4227723@gmail.com>
2018-11-23 14:37:36 +0200

    js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,5 +6,26 @@ $(document).ready(function(){
 
 //                 });
 
+$('.article__more').on('click', function() {
+    $(this).siblings('div.article__content').toggleClass('active');
+    // console.log();
+	});
+
+var location = window.location.href;
+    var cur_url = location.split('/').pop();
+    console.log('location = ' + location);
+    console.log('cur_url = ' + cur_url);
+
+$('.nav-link').each(function() {
+    var link = $(this).attr('href');
+ 	console.log('link=' + link);
+ 	console.log('this=' + this);
+        if (cur_url == link)
+        {
+            $(this).find('.nav-link').addClass('active');
+
+        }
+	});
 	 
-})
+});
+
